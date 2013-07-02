92108980d6f51a70ba5adcc8b8a58a1cf51741a5
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-07-02 12:29:55 +0100

    Sort out graphite-web's python dependencies

--- manifests/install.pp
+++ manifests/install.pp
@@ -27,10 +27,32 @@ class graphite::install {
     require => Exec['graphite/create virtualenv'],
   }
 
+  # Infuriatingly, graphite-web doesn't understand python packaging, so we get
+  # to do its job for it here.
+  ensure_packages([
+    'libffi-dev',
+    'libcairo2-dev',
+  ])
+
+  $graphite_pkgs = [
+    'graphite-web==0.9.10',
+    'Django==1.1.4',
+    'django-tagging==0.3.1',
+    'cairocffi==0.5',
+    'pytz==2013b',
+    'pyparsing==1.5.7'
+  ]
+
+  $graphite_pkgs_str = join($graphite_pkgs, ' ')
+
   exec { 'graphite/install graphite-web':
-    command => '/opt/graphite/bin/pip install graphite-web',
+    command => "/opt/graphite/bin/pip install ${graphite_pkgs_str}",
     creates => '/opt/graphite/webapp/graphite/manage.py',
-    require => Exec['graphite/create virtualenv'],
+    require => [
+      Exec['graphite/create virtualenv'],
+      Package['libffi-dev'],
+      Package['libcairo2-dev'],
+    ],
   }
 
   exec { 'graphite/install gunicorn':
