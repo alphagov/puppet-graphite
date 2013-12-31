28748324f44bfecd2917b4cd6617225d9bd375f7
Dan Carley <dan.carley@gmail.com>
2013-12-31 15:03:25 +0000

    Remove extraenous subscribe from config file
    I'm 99% sure that a `subscribe` has no effect on a file. You can't notify a
    file resource to be evaluated - they are always evaluated. The chaining in
    `init.pp` ensures that `::install` always happens before `::config`.

--- manifests/config.pp
+++ manifests/config.pp
@@ -103,7 +103,6 @@ class graphite::config {
     ensure    => 'directory',
     owner     => 'www-data',
     mode      => '0775',
-    subscribe => Exec['graphite/install graphite-web'],
   }
 
   file { "${root_dir}/webapp/graphite/local_settings.py":
