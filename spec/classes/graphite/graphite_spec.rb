d387f0d3ea1e17d09461cc2858353ce4f466a5a4
Yasser Nabi <yassersaleemi@gmail.com>
2014-10-26 15:11:55 +0000

    Updated Rspec tests
    Rspec tests updated to include tests for added features:
    
    - Managed Graphite user and group
    - Install from OS package, using Package resource.

--- spec/classes/graphite/graphite_spec.rb
+++ spec/classes/graphite/graphite_spec.rb
@@ -2,9 +2,25 @@ require 'spec_helper'
 
 describe 'graphite', :type => :class do
   let(:facts) { {:osfamily => 'debian'} }
-  it { should create_class('graphite::config')}
-  it { should create_class('graphite::install')}
-  it { should create_class('graphite::service')}
 
-  it { should contain_class('graphite::deps') }
+  context 'default params' do
+    it { should create_class('graphite::config')}
+    it { should create_class('graphite::install')}
+    it { should create_class('graphite::service')}
+
+    it { should contain_class('graphite::deps') }
+  end
+
+  context "manage user" do
+    let(:params) {{ :manage_user => true }}
+
+    it { should create_class('graphite::user')}
+  end
+
+  context 'use_python_pip is false' do
+    let(:params) {{ :use_python_pip => false }}
+
+    it { should_not create_class('graphite::deps') }
+  end
+
 end
