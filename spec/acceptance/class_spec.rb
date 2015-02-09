601b3ae5211a2075edba5a285c23017a6468ae8b
Dan Carley <dan.carley@gmail.com>
2015-02-09 13:44:17 +0000

    Extend acceptance tests to cover custom root_dir
    This will prevent us from breaking the functionality in ed5642d again.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,24 +1,12 @@
 require 'spec_helper_acceptance'
 
-describe 'graphite' do
+shared_examples_for "working graphite" do |puppet_manifest|
   it 'should run successfully' do
-    pp = <<-END
-      class { 'python':
-        pip        => true,
-        dev        => true,
-        virtualenv => true,
-      }
-
-      class { 'graphite':
-        require => Class['python'],
-      }
-    END
-
     # Apply twice to ensure no errors the second time.
-    apply_manifest(pp, :catch_failures => true) do |r|
+    apply_manifest(puppet_manifest, :catch_failures => true) do |r|
       expect(r.stderr).not_to match(/error/i)
     end
-    apply_manifest(pp, :catch_failures => true) do |r|
+    apply_manifest(puppet_manifest, :catch_failures => true) do |r|
       expect(r.stderr).not_to match(/error/i)
 
       # ensure idempotency
@@ -42,5 +30,36 @@ describe 'graphite' do
     it 'should serve dashboard without errors' do
       shell('curl -fsS http://localhost:8000/dashboard/')
     end
+  end
+end
+
+describe 'graphite' do
+  context 'default params' do
+    it_should_behave_like "working graphite", <<-END
+      class { 'python':
+        pip        => true,
+        dev        => true,
+        virtualenv => true,
+      }
+
+      class { 'graphite':
+        require => Class['python'],
+      }
+    END
+  end
+
+  context 'custom root_dir' do
+    it_should_behave_like "working graphite", <<-END
+      class { 'python':
+        pip        => true,
+        dev        => true,
+        virtualenv => true,
+      }
+
+      class { 'graphite':
+        root_dir => '/usr/local/graphite',
+        require  => Class['python'],
+      }
+    END
   end
 end
